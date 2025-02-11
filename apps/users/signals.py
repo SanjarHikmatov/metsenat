211b6e69063b182cbc5131cf6a598dbfaf6646a7
SanjarHikmatov <hikmatovsanjar885@gmail.com>
2025-02-11 19:14:51 +0500

    zxcvbnm

--- apps/users/signals.py
+++ apps/users/signals.py
@@ -11,4 +11,4 @@ from apps.users.models import CustomUser
 def post_save_user(instance: CustomUser, created: bool, **kwargs):
     if created:
         instance.add_to_group()
-print('hello world')
+print('hello world yana yangi xatolik')
